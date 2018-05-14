dc75b2d3b8d82aba928ee1aba07d12faa1c8fa84
Kathryn Lawton <klawton16@gmail.com>
2018-05-14 07:57:36 -0500

    adding files

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
 "use strict"
 
-//loop through string
-//compare the current and next char
-//Iif char !=
-//Then increment the count by 1 and do string concant
+
+let string = "aaabbbbbccccaacccbbbaaabbbaaa";
 
 
-function findDuplicateCharacters(input) {
-  // Split the string and count the occurrences of each character
-  var count = input.split('').reduce(function(countMap, word) {
-    countMap[word] = ++countMap[word] || 1;
-    return countMap;
-  }, {});
 
-  // Get the letters that were found, and filter out any that only appear once.
-  var matches = Object.keys(count)
-    .filter(function (key) { return (count[key] > 1); })
-    // Then map it and create a string with the correct length, filled with that letter.
-    .map(function (key) {
-      return new Array(count[key] + 1).join(key);
-    });
 
-  return matches;
+function compressString (string) {
+	let result = "";
+	let count = 1;
+
+	for (let i = 0; i < string.length; i++) {
+		if (string[i] === string[i + 1]) {
+			count ++;
+		} else {
+			 result += count + string[i];
+			 count = 1;
+
+		}	
+	}
+
+	return result;
+
+	// console.log(result);
 }
 
-var results = ['aaaaaaaaaaaaaaaaaabbbccd'].map(findDuplicateCharacters);
+let finalString = compressString(string);
+console.log(finalString);
 
-document.getElementById("results").innerHTML = results.join('<br />');
+
+
+	
